bf41494be684493da1242e54727a1f7930a5bdf2
saikrishna644 <saigundra65@gmail.com>
2025-07-10 09:52:37 +0530

    project completed

--- todoapp/todoproject/settings.py
+++ todoapp/todoproject/settings.py
@@ -65,10 +65,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': 'root',
-        'PASSWORD': 'Saigundra@123',
-        'HOST': 'localhost',
-        'PORT': '3306',
     }
 }
 
